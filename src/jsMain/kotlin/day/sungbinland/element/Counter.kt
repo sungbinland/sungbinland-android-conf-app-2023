e5742e2c119e11ca033260a1dba423aab5c94de3
jisungbin <ji@sungb.in>
2023-01-07 00:54:37 +0900

    feat: full-screen 으로 변경

--- src/jsMain/kotlin/day/sungbinland/element/Counter.kt
+++ src/jsMain/kotlin/day/sungbinland/element/Counter.kt
@@ -8,8 +8,20 @@
 package day.sungbinland.element
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.AlignItems
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.DisplayStyle
+import org.jetbrains.compose.web.css.FlexDirection
+import org.jetbrains.compose.web.css.JustifyContent
+import org.jetbrains.compose.web.css.alignItems
+import org.jetbrains.compose.web.css.backgroundColor
+import org.jetbrains.compose.web.css.display
+import org.jetbrains.compose.web.css.flexDirection
+import org.jetbrains.compose.web.css.height
+import org.jetbrains.compose.web.css.justifyContent
 import org.jetbrains.compose.web.css.padding
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Span
@@ -21,7 +33,16 @@ fun Counter(
     onIncrement: () -> Unit,
     onDecrement: () -> Unit,
 ) {
-    Div({ style { padding(25.px) } }) {
+    Div({
+        style {
+            height(100.vh)
+            backgroundColor(Color.azure)
+            display(DisplayStyle.Flex)
+            alignItems(AlignItems.Center)
+            justifyContent(JustifyContent.Center)
+            flexDirection(FlexDirection.Row)
+        }
+    }) {
         Button(attrs = { onClick { onDecrement() } }) {
             Text("-")
         }
